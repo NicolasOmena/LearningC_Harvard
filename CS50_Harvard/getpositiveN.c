88585433f4540a64d3f65016b0f50fc1d5293a49
nicolasomena <sounicolasomena@gmail.com>
2025-04-06 00:12:58 -0300

    .

--- CS50_Harvard/getpositiveN.c
+++ CS50_Harvard/getpositiveN.c
@@ -5,4 +5,10 @@ int get_positive_int(void){
     int PositiveN;
     do{ PositiveN = get_int("How much meows do you want? ");
     }while (PositiveN < 1);
+    return PositiveN;
+}
+    int main(void){
+        int positiveNumber = get_positive_int();
+        printf("Enter a number: ");
+        scanf("You entered: %d\n", positiveNumber);
 }
